8ff1d08eaa599cc2ff901a22b0c287b140b7204a
RWUBAKWANAYO <68381641+RWUBAKWANAYO@users.noreply.github.com>
2022-04-28 12:57:37 +0200

    remove commented lines in webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,3 @@
-// const path = require('path');
-
-// module.exports = {
-//   mode:'development',
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'main.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-// };
 
  const path = require('path');
  const HtmlWebpackPlugin = require('html-webpack-plugin');
